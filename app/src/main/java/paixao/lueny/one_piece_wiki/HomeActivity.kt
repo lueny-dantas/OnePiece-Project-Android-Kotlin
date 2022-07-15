7fa7421dc5fb9388f2bda219865fdec9e8a51008
Lueny Dantas <luenypaixao@gmail.com>
2022-07-15 00:38:57 -0300

    feat: home activity

---
+++ app/src/main/java/paixao/lueny/one_piece_wiki/HomeActivity.kt
@@ -0,0 +1,15 @@
+package paixao.lueny.one_piece_wiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = View(this)
+        setContentView(R.layout.activity_home)
+        val toolbar = findViewById<Toolbar>(R.id.homeToolbar)
+    }
+}
