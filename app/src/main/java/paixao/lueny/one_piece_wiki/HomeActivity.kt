e7cc245be93841fc136cdec74bc8ef9149a58b78
Lueny Dantas <luenypaixao@gmail.com>
2022-07-19 00:57:02 -0300

    fix: navigate from activity home to activity crew

--- app/src/main/java/paixao/lueny/one_piece_wiki/HomeActivity.kt
+++ app/src/main/java/paixao/lueny/one_piece_wiki/HomeActivity.kt
@@ -1,17 +1,27 @@
 package paixao.lueny.one_piece_wiki
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = View(this)
         setContentView(R.layout.activity_home)
         val toolbar = findViewById<Toolbar>(R.id.homeToolbar)
+        val buttonCrew = findViewById<Button>(R.id.homeCrewButton)
+        val buttonSeason = findViewById<Button>(R.id.homeSeasonButton)
 
+        buttonCrew.setOnClickListener {
+            navigateToCrewActivity()
+        }
+    }
+
+    private fun navigateToCrewActivity(){
+        val intentOfCrewActivity = Intent(this, CrewActivity::class.java)
+        startActivity(intentOfCrewActivity)
     }
 }
