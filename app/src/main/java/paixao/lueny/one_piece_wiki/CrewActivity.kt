b80351a4f15b833440f36a693b00c4dabef815c5
Lueny Dantas <luenypaixao@gmail.com>
2022-07-18 22:13:34 -0300

    feat: activity

---
+++ app/src/main/java/paixao/lueny/one_piece_wiki/CrewActivity.kt
@@ -0,0 +1,12 @@
+package paixao.lueny.one_piece_wiki
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CrewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crew)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+}
+    }
--- app/src/main/java/paixao/lueny/one_piece_wiki/HomeActivity.kt
+++ app/src/main/java/paixao/lueny/one_piece_wiki/HomeActivity.kt
@@ -3,6 +3,7 @@ package paixao.lueny.one_piece_wiki
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
 class HomeActivity : AppCompatActivity() {
@@ -11,5 +12,6 @@ class HomeActivity : AppCompatActivity() {
         val view = View(this)
         setContentView(R.layout.activity_home)
         val toolbar = findViewById<Toolbar>(R.id.homeToolbar)
+
     }
 }
