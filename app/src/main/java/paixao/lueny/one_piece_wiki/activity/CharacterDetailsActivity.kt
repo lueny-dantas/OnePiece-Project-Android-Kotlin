7656cd12a16411cee01fa0d4059d017f0199fda8
Lueny Dantas <luenypaixao@gmail.com>
2022-07-22 01:34:11 -0300

    feat: Character Details Activity Implementation

---
+++ app/src/main/java/paixao/lueny/one_piece_wiki/activity/CharacterDetailsActivity.kt
@@ -0,0 +1,35 @@
+package paixao.lueny.one_piece_wiki.activity
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import paixao.lueny.one_piece_wiki.R
+import paixao.lueny.one_piece_wiki.recyclerview.adapter.Character
+
+class CharacterDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_details)
+
+        val character = intent.getSerializableExtra("character") as? Character
+        if (character != null) {
+            val imageView = findViewById<ImageView>(R.id.characterDetailsImage)
+            imageView.setImageResource(character.image)
+            val nameView = findViewById<TextView>(R.id.characterDetailsName)
+            nameView.text = character.name
+            val roleView = findViewById<TextView>(R.id.characterDetailsRole)
+            roleView.text = character.role
+            val rewardView= findViewById<TextView>(R.id.characterDetailsReward)
+            rewardView.text = character.reward
+            val akumanoMiView = findViewById<TextView>(R.id.characterDetailsAkumanoMi)
+            akumanoMiView.text = character.akumanoMi
+            val powersView = findViewById<TextView>(R.id.characterDetailsPowers)
+            powersView.text = character.powers
+
+        }
+    }
+}
+
+
